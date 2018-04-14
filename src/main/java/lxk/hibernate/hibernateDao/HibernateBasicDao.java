906270c3054add54b99f8541f5c7a02149dfeb81
minarix <minarix@163.com>
2018-04-14 15:14:21 +0800

    all is blank's fault

--- src/main/java/lxk/hibernate/hibernateDao/HibernateBasicDao.java
+++ src/main/java/lxk/hibernate/hibernateDao/HibernateBasicDao.java
@@ -22,8 +22,6 @@ package lxk.hibernate.hibernateDao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import lxk.hibernate.hibernateEntity.HibernateUser;
